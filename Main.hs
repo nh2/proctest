ddc72e46fb388e038145248d6d78bf0364c56e58
Niklas Hambüchen <mail@nh2.me>
2012-08-03 21:17:16 +0100

    Some cleanup and haddock

--- Main.hs
+++ Main.hs
@@ -1,23 +1,43 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-{-# LANGUAGE FlexibleInstances #-}
 
-{-
-Notes when using this module:
+{- | Read this first:
+
   - It needs to be compiled with "-threaded" for not blocking on process starts.
+
   - Beware that the Haskell GC closes process handles after their last use.
     If you don't want to be surprised by this, use hClose where you want
     them to be closed.
+
   - Be sure to use hSetBuffering appropriately, as processes run with BlockBuffering
     by default.
+
   - Dont run the program in a shell (e.g. runInteractiveCommand) if you want to
     be able to terminate it reliably (terminateProcess). Use processes without shells
     (runInteractiveProcess) instead.
 -}
 
--- TODO allow setting hSetBinaryMode
--- TODO throw exception on timeout instead of returning maybe
+module Main (
+  -- * String helpers
+  asUtf8
+, asUtf8Str
 
-module Main where
+ -- * Running and stopping programs
+, run
+, terminateProcesses
+, closeHandles
+
+-- * Time conversion
+, seconds
+, msToTimeoutUs
+
+-- * Communicating with programs
+, TimeoutException
+, waitOutput
+, waitOutputNoEx
+
+, main
+)
+where
 
 import Control.Applicative
 import Control.Concurrent (threadDelay)
@@ -36,23 +56,34 @@ import Test.Hspec.HUnit
 import Test.HUnit
 
 
-
 asUtf8 :: BS.ByteString -> Text
 asUtf8 = decodeUtf8
 
 asUtf8Str :: BS.ByteString -> String
 asUtf8Str = unpack . asUtf8
 
+
+-- | Runs a program with the given arguemtns.
+--
+-- Returns @(stdout, stderr, stdin, process)@. See 'runInteractiveProcess'.
+--
+-- Directly runs the process, does not use a shell.
 run :: FilePath -> [String] -> IO (Handle, Handle, Handle, ProcessHandle)
 run cmd args = runInteractiveProcess cmd args Nothing Nothing
 
+-- | Terminates all processes in the list.
 terminateProcesses :: [ProcessHandle] -> IO ()
 terminateProcesses = mapM_ terminateProcess
 
+-- | Closes all handles in the list.
 closeHandles :: [Handle] -> IO ()
 closeHandles = mapM_ hClose
 
--- | Turns seconds into microseconds (throws error if they don't fit into Int).
+
+-- | Turns seconds into microseconds.
+-- Throws error if they don't fit into Int.
+--
+-- Example: @(seconds 0.2)@ are 200 ms.
 seconds :: Float -> Int
 seconds s = errorOnOverflow (round $ s * 10^6)
   where
@@ -63,36 +94,42 @@ seconds s = errorOnOverflow (round $ s * 10^6)
       | otherwise                          = fromIntegral i
 
 
-_BUFSIZE = 2
-_TIMEOUT_US = 3 * 10^6
-
-mainloop :: Handle -> Handle -> IO ()
-mainloop inh outh = do
-  mr <- timeout _TIMEOUT_US $ BS.hGetSome outh _BUFSIZE
-  case mr of
-    Nothing -> return () -- timeout
-    Just r  -> if r == BS.empty
-                 then putStrLn "empty" >> return ()
-                 else print r >> mainloop inh outh
-
+-- | Turns the given number of milliseconds into microseconds.
+-- Throws error if they don't fit into Int.
 msToTimeoutUs :: Int -> Int
 msToTimeoutUs ms
   | ms > ((maxBound :: Int) `div` 1000) = error "System.Timeout.timeout Int argument overflow"
   | otherwise                           = ms * 1000
 
+
+-- | Exception to be thrown when a program did not terminate
+-- within the expected time.
 data TimeoutException = TimeoutException deriving (Show, Typeable)
 
 instance Exception TimeoutException
 
-waitOutput :: Int -> Int -> Handle -> IO BS.ByteString
-waitOutput timeout_ms maxBytes handle = let ex = throw TimeoutException in
-  waitOutputNoEx timeout_ms maxBytes handle >>= maybe ex return
 
-waitOutputNoEx :: Int -> Int -> Handle -> IO (Maybe BS.ByteString)
+-- | Blocking wait for output on the given handle.
+--
+-- Returns 'Nothing' timeout is exceeded.
+waitOutputNoEx :: Int                       -- ^ Timeout in milliseconds.
+               -> Int                       -- ^ Maximum number of bytes to read.
+               -> Handle                    -- ^ The handle to read from.
+               -> IO (Maybe BS.ByteString)  -- ^ What was read from the handle.
 waitOutputNoEx timeout_ms maxBytes handle =
   timeout (msToTimeoutUs timeout_ms) (BS.hGetSome handle maxBytes)
 
-ignoreTimeout = maybe BS.empty id
+-- | Blocking wait for output on the given handle.
+--
+-- Throws a 'TimeoutException' if the timeout is exceeded.
+--
+-- Based on 'waitOutputNoEx'.
+waitOutput :: Int               -- ^ Timeout in milliseconds.
+           -> Int               -- ^ Maximum number of bytes to read.
+           -> Handle            -- ^ The handle to read from.
+           -> IO BS.ByteString  -- ^ What was read from the handle.
+waitOutput timeout_ms maxBytes handle = let ex = throw TimeoutException in
+  waitOutputNoEx timeout_ms maxBytes handle >>= maybe ex return
 
 
 catTest = do
